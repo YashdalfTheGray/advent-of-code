fcb7d20d60df487e3bcd73480c983be9ce029283
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-10 05:12:38 -0500

    started parsing the input file

---
+++ 2020/day4/passport_processing/src/utils.rs
@@ -0,0 +1,14 @@
+use std::error::Error;
+use std::fs;
+
+pub fn read_into_collection(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
+  let contents = fs::read_to_string(filename)?;
+
+  let parsed = contents
+    .split("\n\n")
+    .map(|s| s.to_string())
+    .map(|s| s.replacen("\n", " ", 100))
+    .collect();
+
+  Ok(parsed)
+}
