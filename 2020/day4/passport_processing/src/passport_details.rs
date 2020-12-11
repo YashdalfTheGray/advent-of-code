5399321f8f99f223d69ab720a4291b8676fa5a56
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-11 04:55:27 -0500

    implemented a trait that parsed passport stuff from string, after lots of onger

--- 2020/day4/passport_processing/src/passport_details.rs
+++ 2020/day4/passport_processing/src/passport_details.rs
@@ -1,13 +1,14 @@
 use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Clone)]
 pub struct PassportParseError {
-    failedString: String,
+    failed_string: String,
 }
 
 impl PassportParseError {
-    fn new(failedString: String) -> PassportParseError {
-        PassportParseError { failedString }
+    fn new(failed_string: String) -> PassportParseError {
+        PassportParseError { failed_string }
     }
 }
 
@@ -16,18 +17,67 @@ impl fmt::Display for PassportParseError {
         write!(
             f,
             "Could not parse \"{}\" into a PassportDetails struct.",
-            self.failedString
+            self.failed_string
         )
     }
 }
 
+#[derive(Debug)]
 pub struct PassportDetails {
-  byr: String,
-  iyr: String,
-  eyr: String,
-  hgt: String,
-  hcl: String,
-  ecl: String,
-  pid: String,
-  cid: String,
+    byr: String,
+    iyr: String,
+    eyr: String,
+    hgt: String,
+    hcl: String,
+    ecl: String,
+    pid: String,
+    cid: String,
+}
+
+impl FromStr for PassportDetails {
+    type Err = PassportParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(' ').map(|s| s.to_string()).collect::<Vec<String>>();
+
+        let mut byr = "".to_string();
+        let mut cid = "".to_string();
+        let mut ecl = "".to_string();
+        let mut eyr = "".to_string();
+        let mut hcl = "".to_string();
+        let mut hgt = "".to_string();
+        let mut iyr = "".to_string();
+        let mut pid = "".to_string();
+        let mut parse_error = false;
+
+        for part in parts {
+            let key_value_pair = part.split(':').collect::<Vec<&str>>();
+            match key_value_pair[..] {
+                ["byr", value, ..] => byr = value.to_string(),
+                ["cid", value, ..] => cid = value.to_string(),
+                ["ecl", value, ..] => ecl = value.to_string(),
+                ["eyr", value, ..] => eyr = value.to_string(),
+                ["hcl", value, ..] => hcl = value.to_string(),
+                ["hgt", value, ..] => hgt = value.to_string(),
+                ["iyr", value, ..] => iyr = value.to_string(),
+                ["pid", value, ..] => pid = value.to_string(),
+                [&_, _, ..] | [_] | [] => parse_error = true,
+            }
+        }
+
+        if parse_error {
+            return Err(PassportParseError::new(s.to_string()));
+        }
+
+        Ok(PassportDetails {
+            byr,
+            cid,
+            ecl,
+            eyr,
+            hcl,
+            hgt,
+            iyr,
+            pid,
+        })
+    }
 }
