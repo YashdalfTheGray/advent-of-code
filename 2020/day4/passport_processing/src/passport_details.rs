a3e2d7f3dcffb28950d5642f615b450acdc451d9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-11 04:44:21 -0500

    changed the indent on the error listing

--- 2020/day4/passport_processing/src/passport_details.rs
+++ 2020/day4/passport_processing/src/passport_details.rs
@@ -2,17 +2,23 @@ use std::fmt;
 
 #[derive(Debug, Clone)]
 pub struct PassportParseError {
-  failedString: String,
+    failedString: String,
+}
+
+impl PassportParseError {
+    fn new(failedString: String) -> PassportParseError {
+        PassportParseError { failedString }
+    }
 }
 
 impl fmt::Display for PassportParseError {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(
-      f,
-      "Could not parse {} into a PassportDetails struct.",
-      self.failedString
-    )
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Could not parse \"{}\" into a PassportDetails struct.",
+            self.failedString
+        )
+    }
 }
 
 pub struct PassportDetails {
