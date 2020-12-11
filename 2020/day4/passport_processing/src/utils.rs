17099ec4acad0e203875dbf85780d868c94dca5b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-11 04:44:49 -0500

    also changed the indent on the utils

--- 2020/day4/passport_processing/src/utils.rs
+++ 2020/day4/passport_processing/src/utils.rs
@@ -2,13 +2,13 @@ use std::error::Error;
 use std::fs;
 
 pub fn read_into_collection(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
-  let contents = fs::read_to_string(filename)?;
+    let contents = fs::read_to_string(filename)?;
 
-  let parsed = contents
-    .split("\n\n")
-    .map(|s| s.to_string())
-    .map(|s| s.replacen("\n", " ", 100))
-    .collect();
+    let parsed = contents
+        .split("\n\n")
+        .map(|s| s.to_string())
+        .map(|s| s.replacen("\n", " ", 100))
+        .collect();
 
-  Ok(parsed)
+    Ok(parsed)
 }
