5e2834ad7e4e356f7953b8c7bae809b4658c30e3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-13 05:31:39 -0500

    added the actual input and a simple file parsing solution

--- 2020/day6/custom_customs/src/main.rs
+++ 2020/day6/custom_customs/src/main.rs
@@ -1,3 +1,14 @@
+use std::process;
+
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let customs_answers = utils::read_into_collection("test-input").unwrap_or_else(|err| {
+        println!("{:?}", err);
+        process::exit(1)
+    });
+
+    customs_answers.iter().for_each(|a| {
+        println!("{}", a);
+    })
 }
