65fa06fe3a9012f857b7b37128c1acac28e005ff
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-14 04:28:26 -0500

    added the solution to the second part of day6

--- 2020/day6/custom_customs/src/main.rs
+++ 2020/day6/custom_customs/src/main.rs
@@ -24,8 +24,40 @@ fn main() {
         .map(|hs| hs.len())
         .collect::<Vec<usize>>();
 
+    let everyone_answers = customs_answers
+        .iter()
+        .map(|s| {
+            let parts = s.split(' ').map(|s| s.to_string()).collect::<Vec<String>>();
+            parts
+        })
+        .map(|parts| {
+            let mut answers_map = HashMap::new();
+            let mut result = vec![];
+
+            parts.iter().for_each(|s| {
+                s.chars().for_each(|c| {
+                    *answers_map.entry(c).or_insert(0) += 1;
+                })
+            });
+
+            answers_map.iter().for_each(|(key, val)| {
+                if *val == parts.len() {
+                    result.push(*key);
+                }
+            });
+
+            result
+        })
+        .map(|r| r.len())
+        .sum::<usize>();
+
     println!(
-        "The sum of all unique answers across all groups in the given input is {}",
+        "The sum of all unique answers across all groups in the given input is {}.",
         unique_answers.iter().sum::<usize>()
-    )
+    );
+
+    println!(
+        "The sum of answers that everyone answered in the given input is {}.",
+        everyone_answers
+    );
 }
