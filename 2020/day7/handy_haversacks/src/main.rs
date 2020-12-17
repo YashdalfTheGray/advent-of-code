df559eba70938f10c8bbc6c9757539224bd34395
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-17 03:11:19 -0500

    messing with the graphs library

--- 2020/day7/handy_haversacks/src/main.rs
+++ 2020/day7/handy_haversacks/src/main.rs
@@ -4,6 +4,11 @@ extern crate regex;
 
 use std::process;
 
+use petgraph::{
+    dot::{Config, Dot},
+    Graph,
+};
+
 mod bag_graph;
 mod utils;
 
@@ -17,5 +22,15 @@ fn main() {
 
     let parsed_bag_rules = bag_graph::get_fully_parsed_bag_rules(partial_bag_rules);
 
+    let mut deps = Graph::<&str, &str>::new();
+    let pg = deps.add_node("petgraph");
+    let fb = deps.add_node("fixedbitset");
+    let qc = deps.add_node("quickcheck");
+    let rand = deps.add_node("rand");
+    let libc = deps.add_node("libc");
+    deps.extend_with_edges(&[(pg, fb), (pg, qc), (qc, rand), (rand, libc), (qc, libc)]);
+
     println!("{:#?}", parsed_bag_rules);
+
+    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeNoLabel]));
 }
