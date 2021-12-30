b5858067da63130ccf31f0e0d5f84f25e806e75b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-12-30 13:46:17 -0500

    added a solution to day20 part2 as well

--- 2021/day20/part2.ts
+++ 2021/day20/part2.ts
@@ -3,7 +3,41 @@
 // https://adventofcode.com/2021/day/20#part2
 // input: day20/input.txt
 
-const d20p2Input = (await Deno.readTextFile('day20/input.txt'))
+import {
+  addBorder,
+  getSquareOfPixels,
+  pixelsToNumber,
+  determineNewDefaultValue,
+} from './pixelUtils.ts';
+
+const d20p2Input = (await Deno.readTextFile('day20/input.txt')).split('\n\n');
+
+const imageProcessingMap = d20p2Input[0].trim().split('');
+const image = d20p2Input[1]
   .split('\n')
-  .filter((l) => !!l);
-console.log(d20p2Input);
+  .filter(Boolean)
+  .map((l) => l.split(''));
+
+const algorithmPasses = 50;
+
+let currentImage = image;
+let defaultValue = '.';
+
+for (let i = 0; i < algorithmPasses; i++) {
+  currentImage = addBorder(currentImage, defaultValue);
+  currentImage = currentImage.map((row, y) => {
+    return row.map((_, x) => {
+      const pixels = getSquareOfPixels(currentImage, x, y, defaultValue);
+      const imageProcessingMapIndex = pixelsToNumber(pixels);
+      return imageProcessingMap[imageProcessingMapIndex];
+    });
+  });
+  defaultValue = determineNewDefaultValue(imageProcessingMap, defaultValue);
+}
+
+const litPixels = currentImage.flat().filter((p) => p === '#').length;
+
+console.log('\n');
+console.log(
+  `There are ${litPixels} lit pixels in the image after ${algorithmPasses} passes.`
+);
