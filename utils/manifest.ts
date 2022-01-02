25ff45995458798220040c69f6376b1e2b56d935
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-01-02 15:33:27 -0500

    added a function that keeps some stats on how many days each language was picked'

---
+++ utils/manifest.ts
@@ -0,0 +1,17 @@
+export function getManifestLocation(year: number) {
+  return `./${year}/manifest.json`;
+}
+
+export function addDayToManifest(
+  day: number,
+  language: string,
+  currentManifest: { [key: string]: number[] } = {}
+) {
+  if (!currentManifest[language]) {
+    currentManifest[language] = [day];
+  } else {
+    currentManifest[language].push(day);
+  }
+
+  return currentManifest;
+}
