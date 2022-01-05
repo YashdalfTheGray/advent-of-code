66a4ba172ea63c217836e49d99c77ccb8be9764b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-01-04 21:21:35 -0500

    export an allLanguages array from the index

---
+++ utils/languages/index.ts
@@ -0,0 +1,9 @@
+import Golang from './golang.ts';
+import JavascriptNode from './JavascriptNode.ts';
+import Ruby from './Ruby.ts';
+import Rust from './Rust.ts';
+import TypescriptDeno from './TypescriptDeno.ts';
+
+const allLanguages = [Golang, JavascriptNode, Ruby, Rust, TypescriptDeno];
+
+export default allLanguages;
