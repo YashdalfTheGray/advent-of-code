c370ad0451989b42219650c3daaf249e8c64a86f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-02-04 22:14:16 -0800

    added something that parses, maybe incomplete

--- 2015/day7/src/logic_node.rs
+++ 2015/day7/src/logic_node.rs
@@ -1,3 +1,12 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_until, take_while},
+    character::is_digit,
+    combinator::rest,
+    complete::take,
+    sequence::separated_pair,
+    Finish, IResult,
+};
 use std::{num::ParseIntError, str::FromStr};
 
 #[derive(Debug)]
@@ -34,16 +43,47 @@ impl<T> LogicNode<T> {
             right,
         }
     }
+
+    fn parse_command(s: &str) -> IResult<&str, (&str, &str)> {
+        let (i, lhs) = take_until(" ")(s)?;
+        let (i, _) = tag(" -> ")(i)?;
+        let (i, rhs) = rest(i)?;
+        Ok((i, (lhs, rhs)))
+    }
+
+    fn computation(s: &str) -> IResult<&str, (&str, &str, &str)> {
+        let (i, lhs) = take_until(" ")(s)?;
+        let (i, operation) = alt((
+            tag("AND"),
+            tag("OR"),
+            tag("NOT"),
+            tag("LSHIFT"),
+            tag("RSHIFT"),
+        ))(i)?;
+        let (i, rhs) = rest(i)?;
+
+        Ok((i, (lhs, operation, rhs)))
+    }
 }
 
 impl FromStr for LogicNode<u16> {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts: Vec<String> = s.split("->").map(|s| s.to_string()).collect();
-        let mut _lhs = parts.pop().unwrap().trim().to_string();
-        let output_wire = parts.pop().unwrap().trim().to_string();
-
-        Ok(Self::new(output_wire, Operation::Identity(0), None, None))
+        match Self::parse_command(s).finish() {
+            Ok((_, (lhs, rhs))) => {
+                // let output_wire = rhs.to_string();
+                // let (i, (op, left, right)) = Self::computation(lhs)?;
+                // Ok(Self::new(output_wire, op, left, right))
+                print!("lhs: {} | rhs: {}", lhs, rhs);
+            }
+            Err(_) => {}
+        };
+        Ok(Self::new(
+            "a".to_string(),
+            Operation::Identity(0),
+            None,
+            None,
+        ))
     }
 }
