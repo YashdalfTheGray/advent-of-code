0e7f4fb3665c284865f4d94ee5e8547b95ce87fc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-12-04 21:39:17 -0800

    added some of the tests for the new part2 functions

--- 2023/day1/solution.spec.rb
+++ 2023/day1/solution.spec.rb
@@ -47,4 +47,77 @@ class SanityTest < Test::Unit::TestCase
     result = Solution.find_file_calibration_value(Solution::INPUT)
     assert_equal(54697, result)
   end
+
+  def test_convert_to_integer_a_word
+    result = Solution.convert_to_integer!('two')
+    assert_equal('2', result)
+  end
+
+  def test_convert_to_integer_a_number
+    result = Solution.convert_to_integer!('3')
+    assert_equal('3', result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_at_the_end
+    result = Solution.find_line_calibration_value_fixed('two1nine')
+    assert_equal(29, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_at_the_end_again
+    result = Solution.find_line_calibration_value_fixed('eightwothree')
+    assert_equal(83, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_in_the_middle
+    result = Solution.find_line_calibration_value_fixed('abcone2threexyz')
+    assert_equal(13, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_overlapping
+    result = Solution.find_line_calibration_value_fixed('xtwone3four')
+    assert_equal(24, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_at_the_end_numbers
+    result = Solution.find_line_calibration_value_fixed('4nineeightseven2')
+    assert_equal(42, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_two_places
+    result = Solution.find_line_calibration_value_fixed('zoneight234')
+    assert_equal(14, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_tricky_tricky
+    result = Solution.find_line_calibration_value_fixed('7pqrstsixteen')
+    assert_equal(76, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_no_last_number
+    result = Solution.find_line_calibration_value_fixed('rlrdkzgnk6mnsbxfkhh')
+    assert_equal(66, result)
+  end
+
+  def test_line_calibration_value_fixed_return_value_no_last_word
+    result = Solution.find_line_calibration_value_fixed('rlrdkzgnkeightmnsbxfkhh')
+    assert_equal(88, result)
+  end
+
+  def test_file_calibration_value_fixed_return_value_sample
+    input = "two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen".split("\n").reject(&:empty?)
+
+    result = Solution.find_file_calibration_value_fixed(input)
+    assert_equal(281, result)
+  end
+
+  # def test_file_calibration_value_fixed_return_value_input
+  #   result = Solution.find_file_calibration_value_fixed(Solution::INPUT)
+  #   assert_equal(54878, result)
+  # end
 end
