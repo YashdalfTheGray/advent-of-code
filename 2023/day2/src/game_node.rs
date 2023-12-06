1a375434e7e6a4a10fd5325353f65bb305cb6c89
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-12-05 21:38:54 -0800

    started the solution for day 2, we'll see how far I get

---
+++ 2023/day2/src/game_node.rs
@@ -0,0 +1,52 @@
+use std::{num::ParseIntError, str::FromStr};
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct CubeSet {
+    pub red: u64,
+    pub green: u64,
+    pub blue: u64,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct GameNode {
+    pub id: String,
+    pub number_of_reveals: u64,
+    pub highest_cubeset: CubeSet,
+    pub reveals: Vec<CubeSet>,
+}
+
+impl GameNode {
+    pub fn blank() -> Self {
+        Self {
+            id: "".to_owned(),
+            number_of_reveals: 0,
+            highest_cubeset: CubeSet {
+                red: 0,
+                green: 0,
+                blue: 0,
+            },
+            reveals: [].to_vec(),
+        }
+    }
+
+    pub fn new(id: String, number_of_reveals: u64) -> Self {
+        Self {
+            id,
+            number_of_reveals,
+            highest_cubeset: CubeSet {
+                red: 0,
+                green: 0,
+                blue: 0,
+            },
+            reveals: [].to_vec(),
+        }
+    }
+}
+
+impl FromStr for GameNode {
+    type Err = ParseIntError;
+
+    fn from_str(_s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::blank())
+    }
+}
